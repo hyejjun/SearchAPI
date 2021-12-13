57f0937f3cd6c98b7fe245b42cf36f30286172ac
hyejjun <hjlee9725@gmail.com>
2021-12-13 21:56:58 +0900

    deleteHtml 함수 hooks로 분리

--- src/components/SearchedList.tsx
+++ src/components/SearchedList.tsx
@@ -1,25 +1,11 @@
 // import Styled from 'styled-components'
+import { deleteHtml } from "../hooks/deleteHtml"
 
 const SearchedList = (props: any) => {
     const { data, clickedLike } = props
 
     const getList = () => {
-        function deleteHtml(htmlTxt: string) {
-            htmlTxt = htmlTxt || '';
-            htmlTxt = htmlTxt.replace(/<\/p>/ig, "</p>\n"); // </p> 태그를 </p> + 엔터로 변경
-            htmlTxt = htmlTxt.replace(/&nbsp;/ig, " "); // 공백
-            htmlTxt = htmlTxt.replace(/<br>/ig, "\n"); // <br>을 엔터로 변경
-            htmlTxt = htmlTxt.replace(/&gt;/ig, ">");
-            htmlTxt = htmlTxt.replace(/&lt;/ig, "<");
-            htmlTxt = htmlTxt.replace(/&#34;/ig, '"');
-            htmlTxt = htmlTxt.replace(/&#39;/ig, "'");
-            htmlTxt = htmlTxt.replace(/&amp;/ig, '&');
-
-
-            // HTML 태그제거 정규표현식 사용
-            htmlTxt = htmlTxt.replace(/<(\/)?([a-zA-Z0-9]*)(\s[a-zA-Z0-9]*=[^>]*)?(\s)*(\/)?>/ig, "");
-            return htmlTxt;
-        }
+        
 
         let list = data.map((v: any, k: any) => {
             return (
